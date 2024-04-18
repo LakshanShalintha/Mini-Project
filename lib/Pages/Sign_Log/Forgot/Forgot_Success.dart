89cb60d0e591330ffe7965722281c6fd0fcc49dd
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-04-18 10:29:11 +0530

    add success reset email

---
+++ lib/Pages/Sign_Log/Forgot/Forgot_Success.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:mini_project/Pages/Home_Page.dart';
+
+class Forgot_Success extends StatelessWidget {
+  const Forgot_Success({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              const SizedBox(height: 80),
+              const Image(image: AssetImage("assets/logos/Forgot_Success.png")),
+              const SizedBox(height: 30),
+              // Add your widgets here
+
+              const Center(
+                child: Text(
+                  "Your Password Successfully Reset!",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  "AudiRAB@gmail.com",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Center(
+                child: Text(
+                  "Congragulation! Your Password Successfuly Reset, You Can Use New Password While LogIn.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 30),
+
+              SizedBox(
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle continue action here
+                    // For example, you can navigate to another page
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blue),
+                    minimumSize:
+                    MaterialStateProperty.all<Size>(const Size(200, 50)),
+                  ),
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
