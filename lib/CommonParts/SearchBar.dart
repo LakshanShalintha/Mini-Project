92b0593d8932fe30f2b35c0057f2ea601e189beb
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-04-23 02:13:23 +0530

    add search bar

---
+++ lib/CommonParts/SearchBar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
