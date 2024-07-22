e339a60fe7fbe9b6c497565f1561c183c07f4a68
LakruwanKavinda <wdmlkdissanayake@std.appsc.sab.ac.lk>
2024-07-22 11:42:11 +0530

    1 Project Setup

--- lib/Pages/Home/Text_to_speech.dart
+++ lib/Pages/Home/Text_to_speech.dart
@@ -1,1 +1,26 @@
 // create Text_to_speech.dart file
+//1 Project Setup
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AudiobookApp());
+}
+
+class AudiobookApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Audiobook App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AudiobookHomePage(),
+    );
+  }
+}
+
+class AudiobookHomePage extends StatefulWidget {
+  @override
+  _AudiobookHomePageState createState() => _AudiobookHomePageState();
+}
