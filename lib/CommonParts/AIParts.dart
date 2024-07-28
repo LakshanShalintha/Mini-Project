ea6e96b61e158de7b0efa160a32fa220f1e10657
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-07-29 00:04:31 +0530

    Edit_gallery-AI_page

--- lib/CommonParts/AIParts.dart
+++ lib/CommonParts/AIParts.dart
@@ -1,22 +1,27 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 class AIServices {
+  // Initialize the GenerativeModel with the API key and model
   final model = GenerativeModel(
     model: "gemini-1.5-pro-latest",
     apiKey: "AIzaSyCVvMSw88d29co-TsEBuREFbXFsaRmrwLU",
   );
 
-  Future<String> generateStory(String data) async {
+  // Method to generate a story based on input data
+  Future<String?> generateStory(String data) async {
+    // Define the prompt with the user's data
     final prompt = """
     I want you to create a detailed audio book story based on the following data: $data.
 
     Here are the guidelines for the story structure:
-    - The story should be approximately 2000 words long.
+    - The story should be approximately 5000 words long and I want to speech this text using elevenlab API.
     """;
 
+    // Create a list of content with the prompt text
     final content = [Content.text(prompt)];
 
     try {
+      // Generate content using the model
       final response = await model.generateContent(
         content,
         generationConfig: GenerationConfig(
@@ -26,15 +31,27 @@ class AIServices {
           topK: 64,
         ),
       );
-      print(response.text);  // This will print the response for debugging
-      return response.text ?? 'No content generated';
+
+      // Check if the response text is not null or empty
+      if (response.text != null && response.text!.isNotEmpty) {
+        print(response.text);  // Print the response for debugging
+        return response.text;
+      } else {
+        return 'No content generated';  // Return a message if no content is generated
+      }
     } catch (e) {
+      print('Error: $e');  // Log the error for debugging
+
+      // Handle specific errors with user-friendly messages
       if (e.toString().contains('Resource has been exhausted')) {
         return 'Quota exceeded. Please try again later.';
+      } else if (e.toString().contains('Invalid API key')) {
+        return 'Invalid API key. Please check your API key and try again.';
+      } else if (e.toString().contains('Network error')) {
+        return 'Network error. Please check your internet connection and try again.';
       } else {
-        return 'An error occurred: $e';
+        return 'An error occurred: $e';  // Return a general error message
       }
     }
   }
 }
-
--- lib/Pages/Home/Gallery.dart
+++ lib/Pages/Home/Gallery.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
 import '../../CommonParts/Nav_Menu.dart';
 import 'Favorite_Page.dart';
 
@@ -29,6 +31,8 @@ class _GalleryState extends State<Gallery> {
     FavoritesManager().loadFavorites().then((_) {
       setState(() {});
     });
+
+    _searchController.addListener(_performSearch);
   }
 
   Future<List<Reference>> fetchPdfReferences(String query) async {
@@ -57,42 +61,51 @@ class _GalleryState extends State<Gallery> {
   }
 
   @override
+  void dispose() {
+    _searchController.removeListener(_performSearch);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Gallery'),
       ),
-      backgroundColor: Colors.purple,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _searchController,
-                        decoration: InputDecoration(
-                          hintText: 'Search in story',
-                          suffixIcon: IconButton(
-                            icon: Icon(Icons.search),
-                            onPressed: _performSearch,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          filled: true,
-                          fillColor: Colors.white,
+      backgroundColor: Colors.blueGrey,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        hintText: 'Search in story',
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: _performSearch,
                         ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              FutureBuilder<List<Reference>>(
+            ),
+            Expanded(
+              child: FutureBuilder<List<Reference>>(
                 future: _pdfRefs,
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
@@ -103,69 +116,71 @@ class _GalleryState extends State<Gallery> {
                     return Center(child: Text('No PDFs available'));
                   } else {
                     final pdfRefs = snapshot.data!;
-                    return GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3, // Number of columns
-                        crossAxisSpacing: 10.0, // Spacing between columns
-                        mainAxisSpacing: 10.0, // Spacing between rows
-                        childAspectRatio: 1.2, // Aspect ratio of grid items
-                      ),
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: pdfRefs.length,
-                      itemBuilder: (context, index) {
-                        final pdfUrl = pdfRefs[index].fullPath;
-                        final fileName = pdfRefs[index].name.split('.pdf')[0];
-                        final isFavorited = FavoritesManager().isFavorite(pdfUrl);
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => PDFViewerScreen(fileRef: pdfRefs[index]),
-                              ),
-                            );
-                          },
-                          child: Card(
-                            child: Stack(
-                              children: [
-                                Center(
-                                  child: Text(fileName),
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0), // Add padding here
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3, // Number of columns
+                          crossAxisSpacing: 10.0, // Spacing between columns
+                          mainAxisSpacing: 10.0, // Spacing between rows
+                          childAspectRatio: 1.2, // Aspect ratio of grid items
+                        ),
+                        shrinkWrap: true,
+                        itemCount: pdfRefs.length,
+                        itemBuilder: (context, index) {
+                          final pdfUrl = pdfRefs[index].fullPath;
+                          final fileName = pdfRefs[index].name.split('.pdf')[0];
+                          final isFavorited = FavoritesManager().isFavorite(pdfUrl);
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => PDFViewerScreen(fileRef: pdfRefs[index]),
                                 ),
-                                Positioned(
-                                  top: 8,
-                                  right: 8,
-                                  child: InkWell(
-                                    onTap: () {
-                                      setState(() {
-                                        if (isFavorited) {
-                                          FavoritesManager().removeFavorite(pdfUrl).then((_) {
-                                            setState(() {});
-                                          });
-                                        } else {
-                                          FavoritesManager().addFavorite(pdfUrl).then((_) {
-                                            setState(() {});
-                                          });
-                                        }
-                                      });
-                                    },
-                                    child: Icon(
-                                      isFavorited ? Icons.favorite : Icons.favorite_border,
-                                      color: isFavorited ? Colors.red : Colors.grey,
-                                      size: 30,
+                              );
+                            },
+                            child: Card(
+                              child: Stack(
+                                children: [
+                                  Center(
+                                    child: Text(fileName),
+                                  ),
+                                  Positioned(
+                                    top: 8,
+                                    right: 8,
+                                    child: InkWell(
+                                      onTap: () {
+                                        setState(() {
+                                          if (isFavorited) {
+                                            FavoritesManager().removeFavorite(pdfUrl).then((_) {
+                                              setState(() {});
+                                            });
+                                          } else {
+                                            FavoritesManager().addFavorite(pdfUrl).then((_) {
+                                              setState(() {});
+                                            });
+                                          }
+                                        });
+                                      },
+                                      child: Icon(
+                                        isFavorited ? Icons.favorite : Icons.favorite_border,
+                                        color: isFavorited ? Colors.red : Colors.grey,
+                                        size: 30,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     );
                   }
                 },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: const NavMenu(),
@@ -173,19 +188,116 @@ class _GalleryState extends State<Gallery> {
   }
 }
 
-class PDFViewerScreen extends StatelessWidget {
+class PDFViewerScreen extends StatefulWidget {
   final Reference fileRef;
 
   const PDFViewerScreen({required this.fileRef, Key? key}) : super(key: key);
 
   @override
+  _PDFViewerScreenState createState() => _PDFViewerScreenState();
+}
+
+class _PDFViewerScreenState extends State<PDFViewerScreen> {
+  AudioPlayer? _audioPlayer;
+  bool _isPlaying = false;
+
+  Future<void> _speakText(String text) async {
+    String apiKey = 'sk_1742f1ccb6df0007e03684881085e6f1ae10e60a5b626140';
+    String voiceId = '21m00Tcm4TlvDq8ikWAM';
+
+    String url = 'https://api.elevenlabs.io/v1/text-to-speech/$voiceId';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'accept': 'audio/mpeg',
+          'xi-api-key': apiKey,
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          "text": text,
+          "model_id": "eleven_monolingual_v1",
+          "voice_settings": {"stability": 0.15, "similarity_boost": 0.75}
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final bytes = response.bodyBytes;
+        _audioPlayer = AudioPlayer();
+        await _audioPlayer!.setAudioSource(
+          ConcatenatingAudioSource(
+            children: [
+              AudioSource.uri(Uri.dataFromBytes(bytes, mimeType: 'audio/mpeg')),
+            ],
+          ),
+        );
+        _audioPlayer!.play();
+        setState(() {
+          _isPlaying = true;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Playback started!')),
+        );
+
+        _audioPlayer!.playerStateStream.listen((state) {
+          if (state.processingState == ProcessingState.completed) {
+            setState(() {
+              _isPlaying = false;
+            });
+          }
+        });
+      } else if (response.statusCode == 401) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Unauthorized: Check your API key')),
+        );
+        print('Unauthorized: Check your API key');
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to load audio: ${response.statusCode}, ${response.reasonPhrase}')),
+        );
+        print('Failed to load audio: ${response.statusCode}, ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred: $e')),
+      );
+      print('An error occurred: $e');
+    }
+  }
+
+  void _stopAudio() async {
+    if (_audioPlayer != null) {
+      await _audioPlayer!.stop();
+      setState(() {
+        _isPlaying = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Playback stopped!')),
+      );
+    }
+  }
+
+  Future<String> _downloadFile(Reference fileRef) async {
+    final directory = await getTemporaryDirectory();
+    final file = File('${directory.path}/${fileRef.name}');
+    await fileRef.writeToFile(file);
+    return file.path;
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(fileRef.name.split('.pdf')[0]),
+        title: Text(widget.fileRef.name.split('.pdf')[0]),
       ),
       body: FutureBuilder<String>(
-        future: _downloadFile(fileRef),
+        future: _downloadFile(widget.fileRef),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -195,19 +307,30 @@ class PDFViewerScreen extends StatelessWidget {
             return Center(child: Text('No file available'));
           } else {
             final localPath = snapshot.data!;
-            return PDFView(
-              filePath: localPath,
+            return Stack(
+              children: [
+                PDFView(
+                  filePath: localPath,
+                ),
+                Positioned(
+                  bottom: 16,
+                  right: 16,
+                  child: FloatingActionButton(
+                    child: Icon(_isPlaying ? Icons.stop : Icons.volume_up),
+                    onPressed: () {
+                      if (_isPlaying) {
+                        _stopAudio();
+                      } else {
+                        _speakText(localPath);
+                      }
+                    },
+                  ),
+                ),
+              ],
             );
           }
         },
       ),
     );
-  }
-
-  Future<String> _downloadFile(Reference fileRef) async {
-    final directory = await getTemporaryDirectory();
-    final file = File('${directory.path}/${fileRef.name}');
-    await fileRef.writeToFile(file);
-    return file.path;
   }
 }
