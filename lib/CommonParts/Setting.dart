a2d860d22ddf7dd06a72c7f19dfc3ecfbc56e7ce
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-07-31 00:36:32 +0530

    Edit_setting-page

--- lib/CommonParts/Setting.dart
+++ lib/CommonParts/Setting.dart
@@ -1,23 +1,144 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart'; // Import your home page here
-import '../Pages/Home/Home_Screen.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../Pages/Home_Page.dart';
-import 'AppBar.dart'; // Import the file where CurvedBackground is defined
-
-// Explicitly import VoidCallback from dart:ui
-import 'dart:ui' show VoidCallback;
-
 import 'Nav_Menu.dart';
+import 'ProfilePage.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  _SettingScreenState createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  File? _profileImage;
+  String? _imageUrl;
+  String? _name;
+  String? _email;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProfileData();
+  }
+
+  Future<void> _loadProfileData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      setState(() {
+        _imageUrl = userDoc['profileImage'];
+        _name = userDoc['name'];
+        _email = userDoc['email'];
+      });
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+      await _uploadImageToFirebase(pickedFile.path);
+    }
+  }
+
+  Future<void> _uploadImageToFirebase(String filePath) async {
+    String fileName = basename(filePath);
+    File file = File(filePath);
+
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        await FirebaseStorage.instance
+            .ref('uploads/${user.uid}/$fileName')
+            .putFile(file);
+
+        String downloadURL = await FirebaseStorage.instance
+            .ref('uploads/${user.uid}/$fileName')
+            .getDownloadURL();
+
+        setState(() {
+          _imageUrl = downloadURL;
+        });
+
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'profileImage': downloadURL,
+        }, SetOptions(merge: true));
+      }
+    } catch (e) {
+      print('Error uploading image: $e');
+    }
+  }
+
+  Future<void> _removeImage() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).update({
+        'profileImage': FieldValue.delete(),
+      });
+
+      setState(() {
+        _imageUrl = null;
+        _profileImage = null;
+      });
+    }
+  }
+
+  void _showImageOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.visibility),
+                title: const Text('View Image'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  _viewImage(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.delete),
+                title: const Text('Remove Image'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  _removeImage();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _viewImage(BuildContext context) {
+    if (_imageUrl != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FullScreenImage(imageUrl: _imageUrl!),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(
-        title: 'Account',
-      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -25,27 +146,54 @@ class SettingScreen extends StatelessWidget {
             CurvedBackground(
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          child: const Icon(
-                            Icons.account_circle_sharp,
-                            size: 66,
-                            color: Colors.white70,
+                  SizedBox(height: 70),
+                  GestureDetector(
+                    onTap: () => _showImageOptions(context),
+                    child: Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 70,
+                          backgroundColor: Colors.white38,
+                          backgroundImage: _imageUrl != null ? NetworkImage(_imageUrl!) : null,
+                          child: _imageUrl == null
+                              ? const Text(
+                            'ADD IMAGE',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          )
+                              : null,
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: GestureDetector(
+                            onTap: _pickImage,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                shape: BoxShape.circle,
+                              ),
+                              padding: const EdgeInsets.all(5),
+                              child: const Icon(
+                                Icons.camera_alt,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(width: 10),
-                      const Text(
-                        'Your Text Here',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    _name ?? '',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
                   ),
                 ],
               ),
@@ -71,10 +219,21 @@ class SettingScreen extends StatelessWidget {
                   ),
                   const SizedBox(height: 10),
                   SettingMenuTitle(
-                    icon: Icons.account_circle,
-                    title: 'Account',
-                    subtitle: 'Manage your account',
-                    onTap: () {},
+                    icon: Icons.person,
+                    title: 'Profile',
+                    subtitle: 'Manage your profile',
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfilePage(
+                              imageUrl: _imageUrl,
+                              name: _name,
+                              email: _email,
+                            )),
+                      );
+                      _loadProfileData(); // Refresh the profile data after returning from the profile page
+                    },
                   ),
                   SettingMenuTitle(
                     icon: Icons.account_circle,
@@ -85,27 +244,6 @@ class SettingScreen extends StatelessWidget {
                   SettingMenuTitle(
                     icon: Icons.account_circle,
                     title: 'Account',
-                    subtitle: 'Manage your account',
-                    onTap: () {},
-                  ),
-
-                  /// App Setting
-                  const SizedBox(height: 30),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'App Setting',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.blue,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  SettingMenuTitle(
-                    icon: Icons.account_circle,
-                    title: 'App',
                     subtitle: 'Manage your account',
                     onTap: () {},
                   ),
@@ -129,7 +267,7 @@ class SettingScreen extends StatelessWidget {
                   child: const Text(
                     'Log Out',
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Colors.white,fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
@@ -147,15 +285,27 @@ class SettingScreen extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Confirm Log Out"),
-          content: Text("Are you sure you want to log out?"),
+          backgroundColor: Colors.black,
+          title: const Text("Log Out",
+          textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+            ),
+          ),
+          content: const Text("Are you sure you want to log out?",
+            style: TextStyle(color: Colors.white),
+          ),
           actions: <Widget>[
             TextButton(
               onPressed: () {
                 // Close the dialog
                 Navigator.of(context).pop();
               },
-              child: Text("Cancel"),
+              child: const Text("Cancel",
+              style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
             TextButton(
               onPressed: () {
@@ -171,8 +321,9 @@ class SettingScreen extends StatelessWidget {
                   ),
                 );
               },
-              child: Text("Log Out",
-              style: TextStyle(color: Colors.red),
+              child: const Text(
+                "Log Out",
+                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
               ),
             ),
           ],
@@ -232,10 +383,15 @@ class CurvedBackground extends StatelessWidget {
     return ClipPath(
       clipper: CustomCurvedEdges(),
       child: Container(
-        color: Colors.purpleAccent,
-        padding: const EdgeInsets.all(0),
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage('assets/images/Home_Page/new story.jpeg'), // Add your background image here
+            fit: BoxFit.cover,
+          ),
+          color: Colors.white10,
+        ),
         child: SizedBox(
-          height: 100,
+          height: 300,
           child: Stack(
             children: [
               Positioned(
@@ -297,6 +453,55 @@ class CircularContainer extends StatelessWidget {
         color: backgroundColor,
       ),
       child: child,
+    );
+  }
+}
+
+class CustomCurvedEdges extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+
+    final firstCurveStart = Offset(0, size.height - 20);
+    final firstCurveEnd = Offset(40, size.height - 20);
+    path.quadraticBezierTo(firstCurveStart.dx, firstCurveStart.dy, firstCurveEnd.dx, firstCurveEnd.dy);
+
+    final secondCurveStart = Offset(0, size.height - 20);
+    final secondCurveEnd = Offset(size.width - 40, size.height - 20);
+    path.quadraticBezierTo(secondCurveStart.dx, secondCurveStart.dy, secondCurveEnd.dx, secondCurveEnd.dy);
+
+    final thirdCurveStart = Offset(size.width, size.height - 20);
+    final thirdCurveEnd = Offset(size.width, size.height);
+    path.quadraticBezierTo(thirdCurveStart.dx, thirdCurveStart.dy, thirdCurveEnd.dx, thirdCurveEnd.dy);
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class FullScreenImage extends StatelessWidget {
+  final String imageUrl;
+
+  const FullScreenImage({Key? key, required this.imageUrl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Image.network(imageUrl),
+      ),
     );
   }
 }
