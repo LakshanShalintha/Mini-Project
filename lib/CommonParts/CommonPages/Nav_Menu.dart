642619659dbe0b786966f1c4265cf035973dfd03
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-08-03 19:12:15 +0530

    modified

--- lib/CommonParts/CommonPages/Nav_Menu.dart
+++ lib/CommonParts/CommonPages/Nav_Menu.dart
@@ -1,3 +1,4 @@
+
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
@@ -93,5 +94,4 @@ class NavMenu extends StatelessWidget {
         }
       },
     );
-  }
-}
+  
